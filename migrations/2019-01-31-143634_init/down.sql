bee3560842213b8c3b8bffefea19c70ff159cf4f
Peter Mezei <mezeipetister@gmail.com>
2019-02-07 18:51:32 +0100

    new version

--- migrations/2019-01-31-143634_init/down.sql
+++
@@ -1,2 +0,0 @@
-DROP TABLE transactions
--- This file should undo anything in `up.sql`
--- migrations/2019-01-31-143634_init/up.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE transactions (
-  id SERIAL PRIMARY KEY NOT NULL,
-  title VARCHAR NOT NULL,
-  debit INTEGER NOT NULL,
-  credit INTEGER NOT NULL,
-  payment INTEGER NOT NULL,
-  time_created timestamptz NOT NULL default current_timestamp
-)
--- Your SQL goes here
