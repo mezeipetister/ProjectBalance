943c1ea0a1a8e7579721697985052450a5d4caff
Peter Mezei <mezeipetister@gmail.com>
2019-02-10 19:14:36 +0100

    nothing important

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,21 @@
 /// Main function
+///
+
 fn main() {
     println!("HelloWorld!");
     for item in 1..100 {
-        println!("{}",item);
-        let a : i32 = item;
+        let _a: i32 = item;
+        println!("{}", _a);
+    }
+    println!("HelloBello");
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn demo_test() {
+        let a = 1;
+        let b = 2;
+        assert_eq!(a + b, 3);
     }
 }
