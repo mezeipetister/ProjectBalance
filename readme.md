357d57b16e6c60d80858b12d9423aa4172499b1e
Peter Mezei <mezeipetister@gmail.com>
2019-02-11 07:37:56 +0100

    readme added

---
+++ readme.md
@@ -0,0 +1,10 @@
+# ProjectBalance
+
+This project has a test purpose to validate a basic double entry book keeping
+MVP for SMEs.
+
+I am currently evaluating using Rust-lang and Emacs.
+
+This project is before alpha stage, so please do not use any element of it, but just for your own risk.
+
+If you whish please report an issue and stay in touch.
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,19 @@
+// Copyright 2019 Peter Mezei
+// https://github.com/mezeipetister
+// For more details please check the readme and LICENSE files enclosed.
+
 /// Main function
+/// Hello bello
 ///
+/// # Examples
+/// ```rust
+/// let a = 1;
+/// let b = 2;
+/// assert_eq!(a+b,3);
+/// ```
+// Hello Bello
+use std::fs::{File, OpenOptions};
+use std::path::Path;
 
 fn main() {
     println!("HelloWorld!");
@@ -7,7 +21,9 @@ fn main() {
         let _a: i32 = item;
         println!("{}", _a);
     }
+    let a = 1 + 2;
     println!("HelloBello");
+    OpenOptions::new().create(true).write(true)
 }
 
 #[cfg(test)]
